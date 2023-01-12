bd870f59d8ab0c6f3104f3762d81a10dc42067a3
AkshanshKanojia <akshanshkanojia@gmail.com>
2023-01-12 17:10:18 +0530

    Added Test Buttons

--- Assets/AkshanshCommonPlugins/Scripts/PluginImplementation/Editor/PluginToolEditor.cs
+++ Assets/AkshanshCommonPlugins/Scripts/PluginImplementation/Editor/PluginToolEditor.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEditor;
-namespace AkshanshKanoji.PluginManager
+using AkshanshKanojia.Inputs.Mobile;
+namespace AkshanshKanojia.PluginManager
 {
     public class PluginToolEditor : EditorWindow
     {
@@ -9,6 +10,21 @@ namespace AkshanshKanoji.PluginManager
         {
             EditorWindow _window = GetWindow<PluginToolEditor>();
             _window.titleContent = new GUIContent("Common Plugins Manager");
+        }
+
+        private void OnGUI()
+        {
+            //EditorGUILayout.LabelField("");
+            if (GUILayout.Button("Generate Mobile Input", GUILayout.Width(150),GUILayout.Height(20)))
+            {
+                GameObject _tempObj = new GameObject("Input manager");
+                _tempObj.AddComponent<MobileInputManager>();
+                Debug.Log("Generated Mobile Input Manager Dummy Object!");
+            }
+            if (GUILayout.Button("Close", GUILayout.Width(100), GUILayout.Height(20)))
+            {
+                GetWindow<PluginToolEditor>().Close();
+            }
         }
     }
 }
