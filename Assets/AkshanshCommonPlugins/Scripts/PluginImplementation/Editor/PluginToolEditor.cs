a25e948dacb55dabfcb8e56e2aac0ea28e8ca3f9
AkshanshKanojia <akshanshkanojia@gmail.com>
2023-01-12 17:41:59 +0530

    Added Custom Folder Generation

--- Assets/AkshanshCommonPlugins/Scripts/PluginImplementation/Editor/PluginToolEditor.cs
+++ Assets/AkshanshCommonPlugins/Scripts/PluginImplementation/Editor/PluginToolEditor.cs
@@ -1,10 +1,13 @@
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
 using AkshanshKanojia.Inputs.Mobile;
 namespace AkshanshKanojia.PluginManager
 {
     public class PluginToolEditor : EditorWindow
     {
+        [SerializeField] List<string> customFolderNames;
+
         [MenuItem("Akshansh/Open Plugin Manager")]
         static void GeneratePluginManager()
         {
@@ -15,9 +18,20 @@ namespace AkshanshKanojia.PluginManager
         private void OnGUI()
         {
             //EditorGUILayout.LabelField("");
-            if (GUILayout.Button("Generate Mobile Input", GUILayout.Width(150),GUILayout.Height(20)))
+            if (GUILayout.Button("Generate My Custome Folders", GUILayout.Width(200), GUILayout.Height(20)))
             {
-                GameObject _tempObj = new GameObject("Input manager");
+                AssetDatabase.CreateFolder("Assets", "Akshansh");
+                AssetDatabase.CreateFolder("Assets/Akshansh", "Scripts");
+                AssetDatabase.CreateFolder("Assets/Akshansh", "Models");
+                AssetDatabase.CreateFolder("Assets/Akshansh", "Animations");
+                AssetDatabase.CreateFolder("Assets/Akshansh", "Prefabs");
+                AssetDatabase.CreateFolder("Assets/Akshansh", "Textures");
+                AssetDatabase.CreateFolder("Assets/Akshansh", "Sprites");
+                AssetDatabase.CreateFolder("Assets/Akshansh", "Materials");
+            }
+            if (GUILayout.Button("Generate Mobile Input", GUILayout.Width(150), GUILayout.Height(20)))
+            {
+                GameObject _tempObj = new GameObject("Mobile Input manager");
                 _tempObj.AddComponent<MobileInputManager>();
                 Debug.Log("Generated Mobile Input Manager Dummy Object!");
             }
